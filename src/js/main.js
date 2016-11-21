04fdb13357f3ecfcfb5c183530341538f6246cee
Thomas Wilburn <news@thomaswilburn.net>
2016-11-21 15:59:45 -0800

    Works for two teams

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,8 @@ require("component-responsive-frame/child");
 require("component-leaflet-map");
 var $ = require("jquery");
 
+var $body = $(document.body);
+
 var gps = require("./lib/geolocation").gps;
 
 const url = "https://script.google.com/macros/s/AKfycbzF0qL4TjsFx_r1pRJ9A8oNUxaZvalWFCiIpfRrHgyeFxPmc_lX/exec";
@@ -14,8 +16,15 @@ var mapElement = document.querySelector("leaflet-map");
 var map = mapElement.map;
 var L = mapElement.leaflet;
 
-var pin = document.querySelector(".pin");
-var here = L.circleMarker();
+var main = document.querySelector("main.interactive");
+var $pins = $(".pin");
+var here = L.circleMarker([0, 0], {
+  fillOpacity: .8,
+  fillColor: "#333",
+  color: "white",
+  clickable: false
+});
+here.bindPopup("You are here");
 
 var dragState = null;
 
@@ -24,15 +33,17 @@ var cancel = e => {
   e.stopPropagation();
 }
 
-pin.addEventListener("dragstart", cancel);
+$pins.on("dragstart", cancel);
 
 var startDrag = function(e) {
+  var pin = e.target;
   var bounds = pin.getBoundingClientRect();
   dragState = {
     x: bounds.left,
     y: bounds.top,
     width: bounds.width,
-    height: bounds.height
+    height: bounds.height,
+    pin: pin
   };
 
   pin.classList.add("grabbing");
@@ -42,19 +53,21 @@ var startDrag = function(e) {
 };
 
 var dragMove = function(e) {
-  var x = e.changedTouches ? e.changedTouches[0].clientX : e.clientX;
-  var y = e.changedTouches ? e.changedTouches[0].clientY : e.clientY;
+  var isTouch = !!e.changedTouches;
+
+  var x = isTouch ? e.changedTouches[0].clientX : e.clientX;
+  var y = isTouch ? e.changedTouches[0].clientY : e.clientY;
   x -= dragState.x;
   y -= dragState.y;
-  y -= dragState.height * 1.5;
+  y -= isTouch ? dragState.height * 1.5 : dragState.height * 1;
   x -= dragState.width / 2;
-  pin.style.transform = `translate(${x}px, ${y}px) scale(2)`;
+  dragState.pin.style.transform = `translate(${x}px, ${y}px) scale(${isTouch ? 2 : 1})`;
 };
 
 var stopDrag = function(e) {
 
-  pin.classList.remove("grabbing");
-  pin.style.transform = "";
+  dragState.pin.classList.remove("grabbing");
+  dragState.pin.style.transform = "";
   ["touchmove", "mousemove"].forEach(event => document.body.removeEventListener(event, dragMove));
   ["touchend", "mouseup"].forEach(event => document.body.removeEventListener(event, stopDrag));
 
@@ -66,9 +79,14 @@ var stopDrag = function(e) {
 var placePin = function(coords) {
   here.setLatLng(coords);
   here.addTo(map);
+  here.openPopup();
+  here.bringToFront();
   map.setView(coords);
 
-  var main = document.querySelector("main.interactive");
+  var team = dragState.pin.getAttribute("team");
+
+  $(".form-container .team-name").html(team == "UW" ? "Huskies" : "Cougars")
+
   var bounds = main.getBoundingClientRect();
   main.style.minHeight = bounds.height + "px";
 
@@ -87,10 +105,12 @@ document.querySelector("button.submit-form").addEventListener("click", function(
 
   var inputs = form.querySelectorAll("input, textarea");
   var data = {
-    method: "setPin"
+    method: "setPin",
+    team: dragState.pin.getAttribute("team")
   };
   inputs.forEach(el => data[el.id] = el.value || el.innerHTML);
 
+  main.style.minHeight = "";
   form.innerHTML = "Submitting your story...";
 
   document.body.setAttribute("stage", "submitting-form");
@@ -115,14 +135,40 @@ cancelButton.addEventListener("click", function(e) {
   document.body.setAttribute("stage", "map");
 });
 
-["touchstart", "mousedown"].forEach(event => pin.addEventListener(event, startDrag));
+$pins.on("touchstart mousedown", startDrag);
 
 document.querySelector(".find-me").addEventListener("click", function(e) {
   gps(function(err, coords) {
     if (err) return console.log(err);
-    placePin({ lat: coords[0], lng: coords[1] });
+    map.setView(coords, 15);
   });
 });
 
 var waBounds = [[49.155, -125.06], [45.55, -116.45]];
 map.fitBounds(waBounds);
+
+window.pins.forEach(function(row) {
+  var size = 14;
+  var marker = L.marker([row.lat, row.lng], {
+    icon: L.divIcon({
+      className: "story-pin " + row.team,
+      iconSize: [size, size],
+      html: row.featured ? "&bigstar;" : ""
+    }),
+    zIndexOffset: row.featured ? 1000 : 1
+  });
+  row.fan_years = row.fan_years || 1;
+  marker.addTo(map);
+  marker.bindPopup(`
+<div class="story">
+  <h1>${row.name}</h1>
+  <ul class="stats">
+    <li> Favorite player: ${row.player || "nobody"}
+    <li> Fan for ${row.fan_years == 1 ? row.fan_years + " year" : row.fan_years + " year"}
+  </ul>
+  <div class="story">
+  ${row.story || ""}
+  </div>
+</div>
+  `)
+})
